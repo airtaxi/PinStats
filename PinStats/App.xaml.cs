68424f4a91e9ee5e64f66dd04c02a54713c2d1f9
Howon Lee <kck4156@kagamine-rin.com>
2023-10-07 19:34:34 +0900

    Added unhandled exception handling feature

--- PinStats/App.xaml.cs
+++ PinStats/App.xaml.cs
@@ -1,5 +1,10 @@
 ﻿using H.NotifyIcon;
 using Microsoft.UI.Xaml;
+using System.Threading.Tasks;
+using System;
+using System.IO;
+using System.ComponentModel.DataAnnotations;
+
 namespace PinStats;
 
 public partial class App : Application
@@ -8,7 +13,34 @@ public partial class App : Application
 
 	public App()
 	{
+		Current.UnhandledException += OnApplicationUnhandledException;
+		AppDomain.CurrentDomain.UnhandledException += OnAppDomainUnhandledException;
+		TaskScheduler.UnobservedTaskException += OnTaskSchedulerUnobservedTaskException;
+
 		InitializeComponent();
+	}
+
+	private void OnTaskSchedulerUnobservedTaskException(object sender, UnobservedTaskExceptionEventArgs e) => WriteException(e.Exception);
+	private void OnAppDomainUnhandledException(object sender, System.UnhandledExceptionEventArgs e) => WriteException(e.ExceptionObject as Exception);
+	private void OnApplicationUnhandledException(object sender, Microsoft.UI.Xaml.UnhandledExceptionEventArgs e)
+	{
+		e.Handled = true;
+		WriteException(e.Exception);
+	}
+
+	private static void WriteException(Exception exception)
+	{
+		var baseDirectory = AppContext.BaseDirectory;
+		var path = Path.Combine(baseDirectory, "error.log");
+
+		if (exception is null)
+		{
+			File.AppendAllText(path, $"[{DateTime.Now:yyyy-MM-dd HH:mm:ss.fff}] UNKNOWN\n({baseDirectory})\n\n");
+			return;
+		}
+
+		var text = $"[{DateTime.Now:yyyy-MM-dd HH:mm:ss.fff}] {exception?.Message ?? "UNKNOWN"}: {exception?.StackTrace ?? "UNKNOWN"}\n({baseDirectory})\n\n";
+		File.AppendAllText(path, text);
 	}
 
 	protected override void OnLaunched(LaunchActivatedEventArgs args)
