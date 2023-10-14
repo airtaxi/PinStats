168df95241c5f8faf992a88f35506e25528753ba
Howon Lee <kck4156@kagamine-rin.com>
2023-10-14 18:13:49 +0900

    Changed Registry Key based method to Task Scheduler based method that doesn't invoke UAC prompt

--- PinStats/Helpers/StartupHelper.cs
+++ PinStats/Helpers/StartupHelper.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Win32;
+using Microsoft.Win32.TaskScheduler;
 using System.Diagnostics;
 
 namespace PinStats.Helpers;
@@ -8,21 +9,39 @@ public static class StartupHelper
 	private const string ProgramName = "PinStats";
 	private const string StartupRegistryKey = @"Software\Microsoft\Windows\CurrentVersion\Run";
 
+	// Delete registry key value that was created by the previous version of the program.
+	static StartupHelper()
+	{
+		using RegistryKey key = Registry.CurrentUser.OpenSubKey(StartupRegistryKey);
+		var hasValue = key.GetValue(ProgramName) != null;
+		if (hasValue) key.DeleteValue(ProgramName, false);
+	}
+
 	public static bool IsStartupProgram {
 		get
 		{
-			using RegistryKey key = Registry.CurrentUser.OpenSubKey(StartupRegistryKey);
-			return key.GetValue(ProgramName) != null;
+			using var taskService = new TaskService();
+			return taskService.FindTask(ProgramName) != null;
 		}
 	}
 
 	public static void SetupStartupProgram()
 	{
-		using RegistryKey key = Registry.CurrentUser.OpenSubKey(StartupRegistryKey, true);
+		using var taskService = new TaskService();
 
-		if (!IsStartupProgram) key.SetValue(ProgramName, Process.GetCurrentProcess().MainModule.FileName);
-		else key.DeleteValue(ProgramName, false);
+		if(IsStartupProgram) taskService.RootFolder.DeleteTask(ProgramName);
+		else
+		{
+			using var taskDefinition = taskService.NewTask();
+			taskDefinition.RegistrationInfo.Description = "Set up " + ProgramName + " to run at startup.";
+			taskDefinition.Principal.RunLevel = TaskRunLevel.Highest;
+		
+			taskDefinition.Triggers.Add(new LogonTrigger());
 
-		key.Close();
+			var programPath = Process.GetCurrentProcess().MainModule.FileName;
+			taskDefinition.Actions.Add(new ExecAction(programPath, null, null));
+
+			taskService.RootFolder.RegisterTaskDefinition(ProgramName, taskDefinition);
+		}
 	}
 }
